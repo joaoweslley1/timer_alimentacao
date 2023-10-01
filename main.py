22b3789f0d81481109ccba2a35e24090e3d9d322
João Weslley <joaoweslley.souza55@gmail.com>
2023-10-01 14:01:23 -0300

    Salvando mudanças

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import PySimpleGUI as sg
 
+
+
 def gera_user():
     '''
     Gerar uma janela contendo o nome do usuário e um temporizador.
     Retorna uma janela com temporizador
     '''
-    added_names=[]
+    
+    added_names=[] # aramazena os nomes que já foram adicionados
+    counter_rows=0
+    clickes=[]
 
-    def gera_linha(nome,pos):
-        return sg.Text(str(nome),size=(25,1),key=str(pos),background_color='gray5')
+    sg.theme('DarkGrey4') # definindo o tema geral
 
+    coluna_fillL=[[sg.Text('',background_color='gray10')]] # preenchimento para alinhamento esquerdo (Left)
+    coluna_fillR=[[sg.Text('',background_color='gray10')]] # preenchimento para alinhamento direito (Right)
 
     coluna1=[
         [sg.Text('Insira um nome:',
@@ -25,31 +31,39 @@ def gera_user():
                    button_color='gray1')]
     ]
 
-    coluna2=[]
+    coluna2=[] # coluna Vazia que receberá os nomes com Timer gerados coma  função gera_coluna
+
     def gera_coluna(nome,num):
-        # parte 1 da coluna 2
+        # parte 1 da coluna 2 (Nome + Timer)
         coluna2a=[[sg.Text(f'Nome: {nome}',
                     size=(20,1),
                     justification='left',
                     key=str(num)+'name',
-                    )
+                    background_color='gray7')
                     ],
             [sg.Text('Tempo: 00:05',
-                    size=(23,1),
+                    size=(20,1),
                     justification='left', 
-                    key=str(num)+'counter')]]
+                    key=str(num)+'counter',
+                    background_color='gray7')]]
         
-        #parte 2 da coluna 2
-        coluna2b=[[sg.Button('X', size=(1,1), key=str(num)+'Exit')]]
+        #parte 2 da coluna 2 (Botão de Excluir)
+        coluna2b=[[sg.Button('X', size=(1,1), key=str(num)+'Exit',button_color='gray7')]]
 
-        lo=[sg.Column(coluna2a,key=str(num)),sg.Column(coluna2b,key=str(num)+'button')]
+        lo=[sg.Column(coluna2a,key=str(num),background_color='grey9'),sg.Column(coluna2b,key=str(num)+'button')] # layout de de saída (LayOut)
 
         return lo
 
-    layout=[[sg.Column(coluna1,
+    layout=[[
+        sg.Column(coluna_fillL,
+                  expand_x=True,
+                  background_color='gray10'),
+        sg.Column(coluna1,
                 background_color='gray5',
-                element_justification='center',
-                ),
+                element_justification='center'),
+        sg.Column(coluna_fillR,
+                  expand_x=True,
+                  background_color='gray10'),
         sg.Column(coluna2,
                 scrollable=True,
                 vertical_scroll_only=True,
@@ -57,17 +71,16 @@ def gera_user():
                 key='Col2',
                 element_justification='center',
                 size=(240,300),
-                ),
+                )
     ]]
 
     window=sg.Window('Janela',
                      layout,
                      background_color='gray10',
-                     element_justification='center')
+                     element_justification='center',
+                     size=(600,300))
     
-    
-    counter_rows=0
-    clickes=[]
+
         
     while True:
         event,values=window.read(timeout=1000)
@@ -99,7 +112,7 @@ def gera_user():
                 try:
                     if window[str(i)+'counter'].get() == 'Tempo: 00:00':        # checa se o tempo zerou
                         nome=str(window[str(i)+'name'].get())[6:]               # salva o nome em uma variável
-                        print(nome)
+                        #print(nome)
                         if nome not in clickes:                                 # checa se o botão de remover já foi clicado
                             window[str(i)+'Exit'].click()                       # realiza o clique o botão de exluir
                             clickes.append(nome)                                # registra o clique realizado
